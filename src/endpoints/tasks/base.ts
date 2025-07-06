9bc755941fad76d079ed4f38a426d9ba4c58c4df
oheteroa <andrei.silchankau@gmail.com>
2025-07-06 11:28:05 +0200

    remove unnecessary source code

--- src/endpoints/tasks/base.ts
+++ src/endpoints/tasks/base.ts
@@ -13,10 +13,11 @@ export const TaskModel = {
   tableName: "tasks",
   primaryKeys: ["id"],
   schema: task,
-  serializer: (obj: Record<string, string | number | boolean>) => {
+  serializer: (obj: object) => {
+    const record = obj as Record<string, string | number | boolean>;
     return {
-      ...obj,
-      completed: Boolean(obj.completed),
+      ...record,
+      completed: Boolean(record.completed),
     };
   },
   serializerObject: task,
